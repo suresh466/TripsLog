bc1bb53bc57c66635c2858eaf380335424a7e7da
Suresh <suresh.thagunna77@gmail.com>
2024-06-13 16:24:15 -0400

    create Trip model, context and setup database connection

---
+++ TripsLog/Models/Trip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TripsLog.Models
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+        public string Destination { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Accommodation { get; set; }
+        public string AccommodationPhone { get; set; }
+        public string AccommodationEmail { get; set; }
+        public string ThingToDo1 { get; set; }
+        public string ThingToDo2 { get; set; }
+        public string ThingToDo3 { get; set; }
+    }
+}
---
+++ TripsLog/Models/TripContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TripsLog.Models
+{
+    // DbContext class for the FAQs database
+    public class TripContext : DbContext
+    {
+        // Constructor to pass default options to the base class
+        public TripContext(DbContextOptions<TripContext> options)
+            : base(options)
+        { }
+        // DbSet properties for the Trip models so CRUD operations can be performed
+        public DbSet<Trip> Trips { get; set; }
+    }
+}
