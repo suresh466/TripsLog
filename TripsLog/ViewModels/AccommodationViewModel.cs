25a1fae4f6032aba5b0aeae14f2ba27aa9b3a182
Suresh <suresh.thagunna77@gmail.com>
2024-06-13 23:17:08 -0400

    validate phone and email

--- TripsLog/ViewModels/AccommodationViewModel.cs
+++ TripsLog/ViewModels/AccommodationViewModel.cs
@@ -1,11 +1,15 @@
-﻿namespace TripsLog.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TripsLog.ViewModels
 {
     public class AccommodationViewModel
     {
         public string Accommodation { get; set; }
 
+        [Phone(ErrorMessage = "Please enter a valid phone number.")]
         public string AccommodationPhone { get; set; }
 
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
         public string AccommodationEmail { get; set; }
     }
 
